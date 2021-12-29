e1f2f3978ab07887c337c423ef0bf3fb287467e3
Jan Czech <czechumennakompie@gmail.com>
2021-12-29 14:44:30 +0100

    Design Patterns
    learning Design Patterns from udemy course

---
+++ ProjectDesign/ProjectDesign/ProjectDesign/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDesign
+{
+    class Person
+    {
+        private string Name { get; set; }
+
+        public Person()
+        {
+            Name = "Noname";
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName() { return Name; }
+
+    }
+}
---
+++ ProjectDesign/ProjectDesign/ProjectDesign/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectDesign
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person("Adam");
+
+            System.Console.WriteLine(person.GetName());
+
+        }
+    }
+}
